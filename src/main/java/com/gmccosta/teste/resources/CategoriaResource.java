8fe8bf31b73a2aaea1e22e63edaa3ede4ab326f0
guicosta98 <guim1998@gmail.com>
2019-07-19 12:17:32 -0300

    Added repository and services

--- src/main/java/com/gmccosta/teste/resources/CategoriaResource.java
+++ src/main/java/com/gmccosta/teste/resources/CategoriaResource.java
@@ -3,29 +3,29 @@ package com.gmccosta.teste.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gmccosta.teste.domain.Categoria;
+import com.gmccosta.teste.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		return ResponseEntity.ok().body(obj);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		
-		return lista;
 	}
 	
 }
