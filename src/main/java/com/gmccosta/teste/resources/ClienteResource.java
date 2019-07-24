a154e73d8b85937ac534eec5f2d2c72d2c7343b9
guicosta98 <guim1998@gmail.com>
2019-07-24 11:05:44 -0300

    Endpoint /clientes/id

---
+++ src/main/java/com/gmccosta/teste/resources/ClienteResource.java
@@ -0,0 +1,28 @@
+package com.gmccosta.teste.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gmccosta.teste.domain.Cliente;
+import com.gmccosta.teste.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+}
